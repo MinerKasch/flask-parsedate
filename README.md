6e3a9264ace6eed9a20f8154e7397893ab5de35d
David Keatts <david.keatts@gmail.com>
2017-03-08 15:32:01 -0800

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # flask-parsedate
+
+JSON endpoint that parses dates.
+
+### Setup
+
+    pip install flask parsedatetime
+    python flask-parsedate.py
+
+### Usage (GET)
+
+    curl http://localhost:5000/?q=<date>
+
+
+### Usage (POST)
+
+    curl -H "Content-type: application/json" -X POST http://127.0.0.1:5000/ -d 'newline delimited series of dates'
---
+++ flask-parsedate.py
@@ -0,0 +1,43 @@
+import json
+import time
+from flask import Flask, Response, request
+import parsedatetime as pdt
+app = Flask(__name__)
+p = pdt.Calendar()
+
+
+HOST='0.0.0.0'
+PORT=5000
+TIME_FORMAT = '%Y-%m-%dT%H:%M:%SZ'
+
+
+def parse_date(query):
+    parse = p.parse(query)[0]
+    date = time.strftime(TIME_FORMAT, parse)
+
+    return date
+
+
+@app.route('/', methods = ['GET', 'POST'])
+def api_root():
+    if request.method == 'GET':
+        date = parse_date(request.args['q'])
+        js = json.dumps({
+                'input': request.args['q'],
+                'output': date
+        })
+
+    elif request.method == 'POST':
+        dates = [parse_date(line) for line in request.js.split('\n')]
+        js = json.dumps({
+                'input': request.js,
+                'output': '\n'.join(dates)
+        })
+
+    resp = Response(js, status=200, mimetype='application/json')
+
+    return resp
+
+
+if __name__ == '__main__':
+    app.run(host=HOST, port=PORT)
