2587013769b85d9e144ddd35a108eb72425875ff
David Keatts <david.keatts@gmail.com>
2017-03-08 16:08:39 -0800

    add redundant date parser for double checking

--- flaskparsedate.py
+++ flaskparsedate.py
@@ -1,5 +1,6 @@
 import json
 import time
+import dateparser
 from flask import Flask, Response, request
 import parsedatetime as pdt
 app = Flask(__name__)
@@ -8,12 +9,22 @@ p = pdt.Calendar()
 
 HOST='0.0.0.0'
 PORT=5000
-TIME_FORMAT = '%Y-%m-%dT%H:%M:%SZ'
+TIME_FORMAT = '%Y-%m-%d'
 
 
-def parse(query):
+def parse1(query):
     parsed = p.parse(query)[0]
     date = time.strftime(TIME_FORMAT, parsed)
+
+    return date
+
+
+def parse2(query):
+    parsed = dateparser.parse(query)
+    try:
+        date = parsed.strftime(TIME_FORMAT)
+    except AttributeError:
+        return None
 
     return date
 
@@ -21,17 +32,33 @@ def parse(query):
 @app.route('/', methods = ['GET', 'POST'])
 def api_root():
     if request.method == 'GET':
-        date = parse(request.args['q'])
+        date1 = parse1(request.args['q'])
+        date2 = parse2(request.args['q'])
+
+        if (date2 is None or date1 != date2):
+            output = 'Error matching parsed dates'
+        else:
+            output = date1
+
         js = json.dumps({
                 'input': request.args['q'],
-                'output': date
+                'output': output
         })
 
     elif request.method == 'POST':
-        dates = [parse(line) for line in request.js.split('\n')]
+        dates1 = [parse1(line) for line in request.js.split('\n')]
+        dates2 = [parse2(line) for line in request.js.split('\n')]
+        output = []
+
+        for i in range(len(dates1)):
+            if dates2[i] is None or dates1[i] != dates2[i]:
+                output.append('Error matching parsed dates')
+            else:
+                output.append(dates1[i])
+
         js = json.dumps({
                 'input': request.js,
-                'output': '\n'.join(dates)
+                'output': output
         })
 
     resp = Response(js, status=200, mimetype='application/json')
